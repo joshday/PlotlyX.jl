276f39cc0f1deb830bc9beceb46b25e107574d6f
Josh Day <emailjoshday@gmail.com>
2024-01-23 12:56:21 -0500

    okay, display works everywhere

--- src/PlotlyX.jl
+++ src/PlotlyX.jl
@@ -2,12 +2,13 @@ module PlotlyX
 
 using Random: randstring
 using Downloads: download
+using REPL
 
 using Scratch, JSON3, EasyConfig, Cobweb, StructTypes
 using Cobweb: h, Node
 
 #-----------------------------------------------------------------------------# exports
-export Plot, Config
+export Plot, Config, Preset
 
 #-----------------------------------------------------------------------------# macros
 macro scratch_path(path...); esc(:(joinpath(Scratch.get_scratch!("assets"), $(path...)))); end
@@ -26,14 +27,14 @@ macro download(url, dest=nothing)
 end
 
 #-----------------------------------------------------------------------------# utils
-get_semver(x) = VersionNumber(match(r"v(\d+)\.(\d+)\.(\d+)", x).match[2:end])
+get_semver(x) = VersionNumber(match(r"(\d+)\.(\d+)\.(\d+)", x).match)
 
 function plotly_latest()
     file = download("https://api.github.com/repos/plotly/plotly.js/releases/latest")
     VersionNumber(JSON3.read(file).name)
 end
 
-template_url(t) = "https://raw.githubusercontent.com/plotly/plotly.py/master/packages/" *
+_template_url(t) = "https://raw.githubusercontent.com/plotly/plotly.py/master/packages/" *
                   "python/plotly/plotly/package_data/templates/$t.json"
 
 const TEMPLATE_NAMES = (:ggplot2, :gridon, :plotly, :plotly_dark, :plotly_white, :presentation,
@@ -43,31 +44,28 @@ fix_matrix(x::Config) = Config(k => fix_matrix(v) for (k,v) in pairs(x))
 fix_matrix(x) = x
 fix_matrix(x::AbstractMatrix) = eachrow(x)
 
-#-----------------------------------------------------------------------------# Assets
-Base.@kwdef struct Assets
-    version::VersionNumber = let
-        file = @scratch_path("plotly.min.js")
-        isfile(file) ? get_semver(readuntil(file, "*/")) : plotly_latest()
-    end
-    url::String = "https://cdn.plot.ly/plotly-$(version).min.js"
-    path::String = @download(url, @scratch_path("plotly.min.js"))
-    template_paths::Dict{Symbol, String} = Dict(t => @download(template_url(t)) for t in TEMPLATE_NAMES)
-    schema_path::String = @download("https://api.plot.ly/v2/plot-schema?format=json&sha1=%27%27", @scratch_path("plot-schema.json"))
-end
+#-----------------------------------------------------------------------------# __init__
+plotly_version::VersionNumber = v"0.0.0"
+plotly_url::String = ""
+plotly_path::String = ""
+plotly_template_paths::Dict{Symbol, String} = Dict()
+plotly_schema_path::String = ""
 
-plotly::Union{Nothing, Assets} = nothing
-
-function update!()
-    Scratch.clear_scratchspaces!(PlotlyX)
-    global plotly = Assets()
+function __init__()
+    global plotly_version = isfile(@scratch_path("plotly.min.js")) ?
+        get_semver(readuntil(@scratch_path("plotly.min.js"), "*/")) :
+        plotly_latest()
+    global plotly_url = "https://cdn.plot.ly/plotly-$(plotly_version).min.js"
+    global plotly_path = @download(plotly_url, @scratch_path("plotly.min.js"))
+    global plotly_template_paths = Dict(t => @download(_template_url(t)) for t in TEMPLATE_NAMES)
+    global plotly_schema_path = @download("https://api.plot.ly/v2/plot-schema?format=json&sha1=%27%27", @scratch_path("plot-schema.json"))
+    global settings = Settings()
     nothing
 end
 
-#-----------------------------------------------------------------------------# __init_)
-function __init__()
-    global plotly = Assets()
-    # preset_auto!()
-    pushdisplay(PlotlyXDisplay())
+function update!()
+    Scratch.clear_scratchspaces!(PlotlyX)
+    __init__()
 end
 
 #-----------------------------------------------------------------------------# Plot
@@ -75,50 +73,30 @@ mutable struct Plot
     data::Vector{Config}
     layout::Config
     config::Config
+    id::String
 end
-Plot(data::Config, layout::Config = Config(), config::Config = Config()) = Plot([data], layout, config)
+Plot(data::Config, layout::Config = Config(), config::Config = Config()) = Plot([data], layout, config, randstring(10))
 Plot(; layout=Config(), config=Config(), @nospecialize(kw...)) = Plot(Config(kw), Config(layout), Config(config))
 (p::Plot)(; @nospecialize(kw...)) = p(Config(kw))
 (p::Plot)(data::Config) = (push!(p.data, data); return p)
 
 StructTypes.StructType(::Plot) = StructTypes.Struct()
-
 Base.:(==)(a::Plot, b::Plot) = all(getfield(a,f) == getfield(b,f) for f in fieldnames(Plot))
 
-
-#-----------------------------------------------------------------------------# Settings
-# Defaults are for REPL and VSCode display
-Base.@kwdef mutable struct Settings
-    pre_div::Function   = () -> h.script(src=plotly.url, charset="utf-8")
-    div::Function       = (id) -> h.div(; id, style="height:100vh;width:100vw;")
-    post_div::Function  = () -> h.div("unused post_div", style="display:none;")
-    layout::Config      = Config()
-    config::Config      = Config(responsive=true)
-    reuse_preview::Bool = true
-end
-
-settings::Settings = Settings()
-
-#-----------------------------------------------------------------------------# display
-struct PlotlyXDisplay <: AbstractDisplay end
-Base.display(::PlotlyXDisplay, o::Plot) = Cobweb.preview(o; reuse=settings.reuse_preview)
-
-function html_div(o::Plot; id=randstring(10))
-    data = fix_matrix.(o.data)
-    layout = merge(settings.layout, o.layout)
-    config = merge(settings.config, o.config)
-    h.div(
-        settings.pre_div(), settings.div(id), settings.post_div(),
-        h.script("""
-            var data = $(JSON3.write(data; allow_inf=true));
-            var layout = $(JSON3.write(layout; allow_inf=true));
-            var config = $(JSON3.write(config; allow_inf=true));
-            Plotly.newPlot("$id", data, layout, config)
-        """)
+#-----------------------------------------------------------------------------# display/show
+function html_div(o::Plot)
+    id = o.id
+    data = JSON3.write(fix_matrix.(o.data); allow_inf=true)
+    layout = JSON3.write(merge(settings.layout, o.layout); allow_inf=true)
+    config = JSON3.write(merge(settings.config, o.config); allow_inf=true)
+    h.div(class="plotlyxjl-parent-div",
+        settings.src,
+        settings.div(; id, class="plotlyxjl-plot-div"),
+        h.script("Plotly.newPlot(\"$id\", $data, $layout, $config)")
     )
 end
 
-html_page(o::Plot; id=randstring(10)) =
+html_page(o::Plot) =
     h.html(
         h.head(
             h.meta(charset="utf-8"),
@@ -127,93 +105,58 @@ html_page(o::Plot; id=randstring(10)) =
             h.title("PlotlyX.jl"),
             h.style("body { margin: 0px; } /* remove scrollbar in iframe */"),
         ),
-        h.body(html_div(o; id))
+        h.body(html_div(o))
     )
 
-function Base.show(io::IO, M::MIME"text/html", o::Plot; id=randstring(10))
-    show(io, M, html_page(o; id))
+html_iframe(o::Plot; kw...) = IFrame(html_page(o); height="450px", width="700px", style="resize:both; display:block; border:none;", kw...)
+
+Base.show(io::IO, ::MIME"juliavscode/html", o::Plot) = show(io, MIME"text/html"(), o)
+
+# Using an <iframe> to display the plot is easier than figuring out what IJulia/Jupyter is doing
+_use_iframe() = (isdefined(Main, :VSCodeServer) && stdout isa Main.VSCodeServer.IJuliaCore.IJuliaStdio) ||
+                (isdefined(Main, :IJulia) && stdout isa Main.IJulia.IJuliaStdio)
+
+Base.show(io::IO, M::MIME"text/html", o::Plot) = show(io, M, _use_iframe() ? html_iframe(o) : html_div(o))
+
+Base.display(::REPL.REPLDisplay, o::Plot) = Cobweb.preview(html_page(o), reuse=settings.reuse_preview)
+
+#-----------------------------------------------------------------------------# Settings
+Base.@kwdef mutable struct Settings
+    src::Cobweb.Node    = h.script(src=plotly_url, charset="utf-8")
+    div::Cobweb.Node    = h.div(; style="height:100%;width:100%;")
+    layout::Config      = Config()
+    config::Config      = Config(responsive=true)
+    reuse_preview::Bool = true
 end
 
-Base.show(io::IO, ::MIME"juliavscode/html", o::Plot) = print(io, settings.generate_html())
+settings = Settings()
 
 
-# #-----------------------------------------------------------------------------# Presets
-# preset_repl!() = (global settings = Settings())
+#-----------------------------------------------------------------------------# Presets
+_template!(t) = (settings.layout.template = JSON3.read(read(plotly_template_paths[t])); nothing)
 
+template_none!()         = haskey(settings.layout, :template) && delete!(settings.layout, :template)
+template_ggplot2!()      = _template!(:ggplot2)
+template_gridon!()       = _template!(:gridon)
+template_plotly!()       = _template!(:plotly)
+template_plotly_dark!()  = _template!(:plotly_dark)
+template_plotly_white!() = _template!(:plotly_white)
+template_presentation!() = _template!(:presentation)
+template_seaborn!()      = _template!(:seaborn)
+template_simple_white!() = _template!(:simple_white)
+template_xgridoff!()     = _template!(:xgridoff)
+template_ygridoff!()     = _template!(:ygridoff)
 
-# function preset_auto!(io::IO = stdout)
-#     global settings = Settings()  # REPL, VSCodeServer.InlineDisplay
-#     if :pluto in keys(io)
-#         settings.div = h.div(id="{id}", style="height:100%;width:100%;")
-#     elseif isdefined(Main, :VSCodeServer) && io isa VSCodeServer.IJuliaCore.IJuliaStdio
-#         settings.parent = (pre_div, div, post-div) = IFrame(h.div(pre_div, div, post_div), width="100%", height="100%")))
-#     end
-#     nothing
-# end
+src_none!() = settings.src = h.div("src_none!", style="display:none;")
+src_cdn!()  = settings.src = h.script(src=plotly_url, charset="utf-8")
+src_local!() = settings.src = h.script(src=plotly_path, charset="utf-8")
+src_standalone!() = settings.src = h.script(read(plotly_path, String), charset="utf-8")
 
+# For Jupyter (doesn't work yet)
+src_require!() = settings.src = h.script(charset="utf-8", type="text/javascript", """
+    require.config({ paths: { plotly: "$plotly_url" } });
+    require(['plotly'], function(Plotly) { window.Plotly = Plotly; });
+    """)
 
-
-# function preset_container_fillwindow!()
-#     settings.config.responsive = true
-#     settings.print_div = (io, id) -> print(io, "    <div style=\"height:100vh;width:100vw;\" id=\"", id, "\"/>")
-# end
-
-# function print_iframe(io::IO, id::String)
-#     buf = IOBuffer()
-#     preset_container_fillwindow!()
-#     print_html_page(buf,)
-# end
-
-# function preset_container_iframe!(height="400px", width="750px")
-#     settings.config.responsive = true
-
-# end
-
-
-
-#
-# #-----------------------------------------------------------------------------# Display
-# struct PlotlyXDisplay <: AbstractDisplay end
-# Base.display(::PlotlyXDisplay, o::Plot) = Cobweb.preview(o; reuse=settings.reuse_preview)
-
-# fix_matrix(x::Config) = Config(k => fix_matrix(v) for (k,v) in pairs(x))
-# fix_matrix(x) = x
-# fix_matrix(x::AbstractMatrix) = eachrow(x)
-
-
-
-# function print_html_page(io::IO, o::Plot; id=randstring(10))
-#     print(io, """
-#     <!doctype html>
-#     <html>
-#       <head>
-#         <meta charset="utf-8">
-#         <meta name="viewport" content="width=device-width, initial-scale=1">
-#         <meta name="description" content="PlotlyX.jl">
-#         <title>PlotlyX.jl</title>
-#         <style>body { margin: 0px; } /* remove scrollbar in iframe */</style>
-#       </head>
-#       <body>
-#     """)
-#     print_html_div(io, o; id=id)
-#     print(io, """
-#       </body>
-#     </html>
-#     """)
-# end
-
-# srcdoc(o::Plot) = (buf = IOBuffer(); print_html_page(buf, o); escape(String(take!(buf))))
-
-# function print_iframe(io::IO, o::Plot; height="400px", width="750px")
-#     id = "plotlyx-iframe-$(randstring(10))"
-#     print(io, "<iframe id=\"$id\" style=\"height:$height;width:$width;border:none;\" srcdoc=\"", srcdoc(o), "\"></iframe>")
-# end
-
-
-# function Base.show(io::IO, M::MIME"text/html", o::Plot)
-#     print_iframe(io, o)
-# end
-
-# Base.show(io::IO, ::MIME"juliavscode/html", o::Plot) = print_html_page(io, o)
 
 end # module
--- test/plotlylight_pluto_test.jl
+++ test/plotlylight_pluto_test.jl
@@ -10,14 +10,14 @@ using Pkg; Pkg.activate("..")
 # ╔═╡ c27879d8-9702-11eb-3050-f1c4477273fe
 begin
 	using Revise
-	using PlotlyLight
-	
-	Preset.PlotContainer.responsive!()
-	Preset.Template.plotly_dark!()
+	using PlotlyX
 end
 
 # ╔═╡ e2dedee2-9702-11eb-1364-118bcaff2607
-Plot(Config(x=1:50, y=randn(50), type="bar"))
+begin
+	PlotlyX.template_ggplot2!()
+	Plot(Config(x=1:50, y=randn(50), type="bar"))
+end
 
 # ╔═╡ Cell order:
 # ╠═14f719cd-ffdc-4d9b-83f4-efd7e7b54c82
