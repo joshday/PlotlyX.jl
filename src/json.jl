604bcd2cbf6e6dc247da910d7e4143ad4e616246
Josh Day <emailjoshday@gmail.com>
2024-11-20 15:36:29 -0500

    Unitful ext

--- src/json.jl
+++ src/json.jl
@@ -1,7 +1,6 @@
-function json_join(io::IO, itr, x)
-    first = true
-    for item in itr
-        first ? (first = false) : print(io, x)
+function json_join(io::IO, itr, sep)
+    for (i, item) in enumerate(itr)
+        i == 1 || print(io, sep)
         json(io, item)
     end
 end
@@ -10,19 +9,24 @@ json(x) = sprint(json, x)
 
 json(io::IO, args...) = foreach(x -> json(io, x), args)
 
+# Strings
 json(io::IO, x::Union{AbstractString, Symbol, AbstractChar}) = print(io, '"', x, '"')
+
+# Numbers
 json(io::IO, x::Real) = print(io, x)
 json(io::IO, x::Rational) = json(io, float(x))
+
+# Null
 json(io::IO, ::Union{Missing, Nothing}) = print(io, "null")
-json(io::IO, x::Pair) = (json(io, string(x.first)); print(io, ':'); json(io, x.second))
+
+# Bool
+json(io::IO, x::Bool) = print(io, x ? "true" : "false")
+
+# Arrays
 json(io::IO, x::AbstractVector) = (print(io, '['); json_join(io, x, ','); print(io, ']'))
 json(io::IO, x::AbstractArray) = json(io, eachslice(x; dims=1))
-json(io::IO, x) = (print(io, '['); join(io, x, ','); print(io, ']'))
+json(io::IO, x) = (print(io, '['); json_join(io, x, ','); print(io, ']'))  # ← FALLBACK METHOD
+
+# Objects
+json(io::IO, x::Pair) = (json(io, string(x.first)); print(io, ':'); json(io, x.second))
 json(io::IO, x::Union{NamedTuple, AbstractDict}) = (print(io, '{'); json_join(io, pairs(x), ','); print(io, '}'))
-
-struct JSON{T}
-    content::T
-end
-json(io::IO, x::JSON) = print(io, x.content)
-
-macro json_str(x); :(JSON($x)) end
